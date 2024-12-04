147db77c5807ae3cbef1de2e3ac25d7f84d22433
akunkel81 <ankunkel13@gmail.com>
2024-12-04 10:12:40 -0500

    Delete lab011 directory

--- lab011/group_by.py
+++
@@ -1,8 +0,0 @@
-def group_by(func, target_list: list):
-    from itertools import groupby
-    target_list_sorted = sorted(target_list, key=func)
-    grouped = groupby(target_list_sorted, key=func)
-    return {k: list(v) for k, v in grouped}
-
-# Examples
-print(group_by(len, ["hi", "dog", "me", "bad", "good"]))
